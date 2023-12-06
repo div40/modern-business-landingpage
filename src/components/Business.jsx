62e6be7b70dba05c04e56a976c04d9a5b7453c66
div40 <vidh40@gmail.com>
2023-12-06 18:04:19 +0530

    nav and hero section

---
+++ src/components/Business.jsx
@@ -0,0 +1,8 @@
+import { features } from "../constants";
+import styles, { layout } from "../style";
+
+const Business = () => {
+  return <div>Business</div>;
+};
+
+export default Business;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { navLinks } from "../constants";
+import { close, logo, menu } from "../assets";
+
+const Navbar = () => {
+  // state for menu icon to change when open or close
+  const [toggle, setToggle] = useState(false);
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="logo" className="w-[124px] h-[32px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        {/* menu icon state usage */}
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 w-full rounded-none h-[50%] z-[100] sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-center items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                } text-white`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
